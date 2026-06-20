61ef327b4a33789f903c968377ba72764c77807b
Larineon <sweetbun159@gmail.com>
2026-06-20 21:24:09 +1000

    Update test_navigate_from_account_to_constructor.py

--- tests/test_navigate_from_account_to_constructor.py
+++ tests/test_navigate_from_account_to_constructor.py
@@ -1,31 +1,36 @@
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By  
+from locators import MainPageLocators
+from data import Urls
+
 
 class TestNavigateFromAccount:
-    
-    def test_click_constructor(self, authorized_main_page):        #Переход из личного кабинета в конструктор по клику на 'Конструктор
+
+    def test_click_constructor(self, authorized_main_page):
+        # Переход из ЛК в конструктор по клику на «Конструктор» 
+
         # Переход в личный кабинет
-        authorized_main_page.find_element(By.XPATH, "//p[text()='Личный Кабинет']").click()
-        
+        authorized_main_page.find_element(*MainPageLocators.PERSONAL_ACCOUNT_BUTTON).click()
+
         # Клик по кнопке "Конструктор"
-        authorized_main_page.find_element(By.XPATH, "//p[text()='Конструктор']").click()
-        
+        authorized_main_page.find_element(*MainPageLocators.CONSTRUCTOR_BUTTON).click()
+
         # Проверка: появился заголовок "Соберите бургер"
-        WebDriverWait(authorized_main_page, 5).until(
+        assert WebDriverWait(authorized_main_page, 5).until(
             EC.visibility_of_element_located((By.XPATH, "//h1[contains(text(),'Соберите бургер')]"))
-        )
-        assert authorized_main_page.find_element(By.XPATH, "//h1[contains(text(),'Соберите бургер')]").is_displayed()
-    
-    def test_click_logo(self, authorized_main_page):        #Переход из личного кабинета в конструктор по клику на логотип
+        ).is_displayed()
+
+    def test_click_logo(self, authorized_main_page):
+        # Переход из ЛК на главную по клику на логотип 
+
         # Переход в личный кабинет
-        authorized_main_page.find_element(By.XPATH, "//p[text()='Личный Кабинет']").click()
-        
+        authorized_main_page.find_element(*MainPageLocators.PERSONAL_ACCOUNT_BUTTON).click()
+
         # Клик по логотипу
-        authorized_main_page.find_element(By.CLASS_NAME, "AppHeader_header__logo__2D0X2").click()
-        
+        authorized_main_page.find_element(*MainPageLocators.LOGO).click()
+
         # Проверка: появился заголовок "Соберите бургер"
-        WebDriverWait(authorized_main_page, 5).until(
+        assert WebDriverWait(authorized_main_page, 5).until(
             EC.visibility_of_element_located((By.XPATH, "//h1[contains(text(),'Соберите бургер')]"))
-        )
-        assert authorized_main_page.find_element(By.XPATH, "//h1[contains(text(),'Соберите бургер')]").is_displayed()
+        ).is_displayed()
